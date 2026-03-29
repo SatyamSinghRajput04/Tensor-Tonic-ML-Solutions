107cf4d8900e310b4268d431ba5af1d4e5ae03ce
satyam singh rajput <ssrajput7474@gmail.com>
2026-03-29 12:29:43 +0530

    Update solution for matrix-factorization-sgd-step

--- matrix-factorization-sgd-step/matrix-factorization-sgd-step.py
+++ matrix-factorization-sgd-step/matrix-factorization-sgd-step.py
@@ -3,6 +3,33 @@ def matrix_factorization_sgd_step(U, V, r, lr, reg):
     Perform one SGD step for matrix factorization.
     """
     # Write code here
+    # U = np.array(U, dtype=float)
+    # V = np.array(V, dtype=float)
+
+    
+    # pred = U[0]*V[0] + U[1]*V[1]
+
+    
+    # e = r - pred
+
+    
+    # U0 = U[0]
+    # U1 = U[1]
+    # V0 = V[0]
+    # V1 = V[1]
+
+  
+    # U[0] = U0 + lr * (e * V0 - reg * U0)
+    # U[1] = U1 + lr * (e * V1 - reg * U1)
+
+    
+    # V[0] = V0 + lr * (e * U0 - reg * V0)
+    # V[1] = V1 + lr * (e * U1 - reg * V1)
+
+    # return U , V
+
+
+    
     U = np.array(U , dtype = float)
     V = np.array(V , dtype = float)
 
